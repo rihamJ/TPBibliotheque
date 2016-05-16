c7ceda2de8c91020f4f0b0bf5af7c69682cc4533
acordier@wall-e <Amelie@192.168.1.2>
2016-05-16 23:11:36 +0200

    Le code métier de la classe ListePersonnes

--- src/bibliotheque/ListePersonnes.java
+++ src/bibliotheque/ListePersonnes.java
@@ -5,6 +5,8 @@
 package bibliotheque;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 /**
  * Classe Personne
@@ -22,19 +24,34 @@ public class ListePersonnes {
     }
     
     public void ajouterPersonne(Personne p){
-        
+        listepers.add(p);
     }
     
     public boolean appartient(Personne p){
-        return false;
+        return listepers.contains(p);
     }
  
     public boolean appartient(String nom, String prenom){
-        return false;
+        boolean present = false;
+        Iterator<Personne> li = listepers.iterator();
+        while(li.hasNext()){
+            Personne p = li.next();
+            if(p.getNom()==nom && p.getPrenom()==prenom){
+                present = true;
+            }
+        }
+        return present;
     }
     
     public void afficherAppartenance(Personne p){
-        System.out.println("appartenance");
+        if(this.appartient(p)){
+            System.out.println("La personne demandée appartient bien à la liste (" + p.getNom() + ", " + p.getPrenom()+").");
+        } else {
+            System.out.println("La personne demandée n'appartient pas à la liste");
+        }
+    }
+    public int getNbPersonnes(){
+        return listepers.size();
     }
     
 }
