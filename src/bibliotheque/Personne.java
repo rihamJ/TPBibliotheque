487294e799fcea9c4ce2153260d7ee8e556e6b94
acordier@wall-e <>
2016-05-16 17:46:17 +0200

    Création de la classe personne

---
+++ src/bibliotheque/Personne.java
@@ -0,0 +1,16 @@
+/**
+ * Bibliothèque
+ * TP CVDA 2016 - Amélie Cordier
+ */
+package bibliotheque;
+
+/**
+ * Classe Personne
+ * @author Amélie Cordier - IUT Lyon 1
+ * @version 1.0
+ * mai 2016
+ */
+
+public class Personne {
+    
+}
