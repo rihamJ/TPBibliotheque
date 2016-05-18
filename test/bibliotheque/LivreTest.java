ce4e8847a45bc0053e84a85b4c35725ee0376f8e
acordier@wall-e <Amelie@192.168.1.2>
2016-05-18 17:03:08 +0200

    Jeu de tests pour la classe Livre

---
+++ test/bibliotheque/LivreTest.java
@@ -0,0 +1,127 @@
+/**
+ * Bibliothèque
+ * TP CVDA 2016 - Amélie Cordier
+ */
+package bibliotheque;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+
+/**
+ * Classe PersonneTest
+ * @author Amélie Cordier   
+ */
+public class LivreTest {
+    
+    static Personne alan;
+    static Livre computing;
+    public LivreTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        alan = new Personne("Turing", "Alan", 1912);
+        computing = new Livre("Computing Machinery and Intelligence", 250, alan);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getNumLivre method, of class Livre.
+     */
+    @Test
+    public void testGetNumLivre() {
+        assertEquals(1, computing.getNumLivre());
+    }
+
+    /**
+     * Test of getTitre method, of class Livre.
+     */
+    @Test
+    public void testGetTitre() {
+        String titreAttendu = "Computing Machinery and Intelligence";
+        assertTrue(computing.getTitre().equals(titreAttendu));
+    }
+
+    /**
+     * Test of getNombreDePages method, of class Livre.
+     */
+    @Test
+    public void testGetNombreDePages() {
+        assertEquals(250, computing.getNombreDePages());
+    }
+
+    /**
+     * Test of getAuteur method, of class Livre.
+     */
+    @Test
+    public void testGetAuteur() {
+        assertEquals(alan, computing.getAuteur());
+    }
+
+    /**
+     * Test of setNumLivre method, of class Livre.
+     */
+    @Ignore
+    @Test
+    public void testSetNumLivre() {
+        fail("Ce test sera traité plus tard");
+    }
+
+    /**
+     * Test of setTitre method, of class Livre.
+     */
+    @Test
+    public void testSetTitre() {
+        Livre mind = new Livre("?", 0, alan);
+        mind.setTitre("Mind");
+        assertTrue(mind.getTitre().equals("Mind"));
+    }
+
+    /**
+     * Test of setAuteur method, of class Livre.
+     */
+    @Test
+    public void testSetAuteur() {
+        Livre ged = new Livre("?", 0, alan);
+        Personne douglas = new Personne("Hofstadter", "Douglas", 1945);  
+        ged.setAuteur(douglas);
+        assertTrue(ged.getAuteur().equals(douglas));
+    }
+
+    /**
+     * Test of setNombreDePages method, of class Livre.
+     */
+    @Test
+    public void testSetNombreDePages() {
+        Livre mind = new Livre("?", 0, alan);
+        mind.setNombreDePages(500);
+        assertEquals(500, mind.getNombreDePages());
+    }
+
+    /**
+     * Test of toString method, of class Livre.
+     */
+    @Test
+    public void testToString() {
+        String expStr = "Computing Machinery and Intelligence, Alan Turing, 250p.";
+        assertTrue(computing.toString().equals(expStr));
+    }
+}
+    
